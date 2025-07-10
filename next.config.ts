7cf669ea4e4049e40aa86862b40c1e197d946084
Samuel Jenkins <samueljenkins@Samuels-MacBook-Air.attlocal.net>
2025-07-10 11:11:33 -0500

    Your commit message

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
